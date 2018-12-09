a307e04a170dfb883577ef7c1aa6f311678ed937
taurus <taoluwork@gmail.com>
2018-12-09 01:10:54 -0600

    aa

--- bcai_v0.9/test/showStatus.js
+++ bcai_v0.9/test/showStatus.js
@@ -56,8 +56,9 @@ web3.eth.getAccounts().then(function(myAccounts){
 		} else { 
 			console.log("=================================================================");
 			console.log(event.event, "  ==>  ", event.blockNumber);
-			console.log("info: ", web3.utils.hexToAscii(web3.utils.bytesToHex(event.returnValues[2])));
 			console.log("info: ", event.returnValues[2]);
+			//console.log("info: ", web3.utils.hexToAscii(web3.utils.bytesToHex(event.returnValues[2])));
+			
 
 			console.log(event.returnValues);
 		}	
