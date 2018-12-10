7c7660284f22aedb3eed4542e9234a57d71da00b
taurus <taoluwork@gmail.com>
2018-12-09 21:40:21 -0600

    aa

--- bcai_v0.9/test/client.js
+++ bcai_v0.9/test/client.js
@@ -193,8 +193,8 @@ function userFireMessage(){
             console.log("Request Submitted! Block: ",ret.blockNumber);
             console.log("-----------------------------------------------------------------")
             if(argv['recpt'])  console.log("Receipt:    <=====######", ret);
-            console.log(ret.events[0])
-            if(ret.events[0] == undefined) throw 'Submit request failed!'
+            console.log(ret.events['SystemInfo'])
+            if(ret.events['SystemInfo'] == undefined) throw 'Submit request failed!'
                     
         }).catch(function(err){
             console.log("Check receipt by --recpt");
