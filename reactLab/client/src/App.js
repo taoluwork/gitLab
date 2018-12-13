f68c4f3105605d10924c7fadc160f649c539a5ef
Taurus <taoluwork@gmail.com>
2018-12-13 13:48:21 -0600

    reactlab added file reader example

--- reactLab/client/src/App.js
+++ reactLab/client/src/App.js
@@ -6,7 +6,11 @@ import truffleContract from "truffle-contract";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { storageValue: 0, 
+    web3: null, 
+    accounts: null, 
+    contract: null,
+    content: null };
 
   componentDidMount = async () => {
     try {
@@ -46,6 +50,22 @@ class App extends Component {
     this.setState({ storageValue: response.toNumber() });
   };
 
+  handleSubmit(event){
+    event.preventDefault();
+  }
+  handelChange(event){
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsText(file);
+    reader.onloadend = ()=>{
+      console.log(reader.result);
+      this.setState({content: reader.result})
+      console.log("I have read the file: ", this.state.content)
+    }
+  }
+
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
@@ -54,15 +74,10 @@ class App extends Component {
       <div className="App">
         <h1>Good to Go!</h1>
         <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+            <input type="file" onChange={this.handelChange.bind(this)}/>
+            <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
