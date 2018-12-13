ae40f645fc8bafecf68d3903839158609409983c
Taurus <taoluwork@gmail.com>
2018-12-13 13:15:17 -0600

    ipfsLab is working

--- ipfsLab/client/src/App.js
+++ ipfsLab/client/src/App.js
@@ -10,12 +10,14 @@ class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
   constructor(props){
     super(props)
+    
     this.state = {
       ipfsHash: '',
       storageValue: 0, 
       web3: null, 
       buffer: null}
-
+    
+      //this.componentDidMount();
     this.captureFile = this.captureFile.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
@@ -25,19 +27,24 @@ class App extends Component {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
+      //console.log(web3);
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
+      //console.log(accounts);
       // Get the contract instance.
       const Contract = truffleContract(SimpleStorageContract);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
-
+      //console.log(instance);
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
+      return instance.get.call(accounts[0]).then((ipfsHash)=>{
+        return this.setState({ipfsHash})
+      })
+    }
+     
+    catch (error) {
       // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`
@@ -59,13 +66,16 @@ class App extends Component {
     this.setState({ storageValue: response.toNumber() });
   };
 
+  //file readers: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+
+
   captureFile(event){
     console.log("capture file");
     event.preventDefault()
     const file = event.target.files[0]
     const reader = new window.FileReader()
     reader.readAsArrayBuffer(file)
-    reader.onloadered = () =>{
+    reader.onloadend = () =>{
       this.setState({buffer: Buffer(reader.result)})
       console.log("buffer", this.state.buffer);
     }
@@ -76,18 +86,28 @@ class App extends Component {
     event.preventDefault();
     ipfs.files.add(this.state.buffer, (err, result)=>{
       if(err){
-        console.log(err);
+        console.log("Error!", err);
         return
       }
       else{
-        return this.setState({ipfsHash: result[0].hash})
+        
+        //this.setState({ipfsHash: result[0].hash})
+        console.log("ipfsHash returned", result[0].hash)
+        return this.state.contract.set(result[0].hash, {from: this.state.accounts[0]}).then((res)=>{
+          console.log("Send", res);
+          return this.state.contract.get.call({from: this.state.accounts[0]}).then((ipfsHash)=>{
+            console.log("Fetched", ipfsHash);
+            this.setState({ipfsHash})
+            
+          })
+        })
       }
     })
     
   }  
 
 
-
+  //components of react: https://reactjs.org/docs/forms.html
 
   render() {
     if (!this.state.web3) {
@@ -97,11 +117,13 @@ class App extends Component {
       <div className="App">
         <h1>Your Image</h1>
         <p>Your image is saved on IPFS and Ethereum blockchain</p>
-        
+        <img src={`https://ipfs.infura.io/ipfs/${this.state.ipfsHash}`} alt="" />
         <h2>Upload Image</h2>
-        <form  onSubmit = {this.onSubmit}></form>
+        <form  onSubmit = {this.onSubmit}>
           <input type = 'file' onChange = {this.captureFile}></input>
-          <input type = 'submit' ></input>
+          <input type = 'submit' value = "Click"></input>
+        </form>
+        
         <p>
           If your contracts compiled and migrated successfully, below will show
           a stored value of 5 (by default).
