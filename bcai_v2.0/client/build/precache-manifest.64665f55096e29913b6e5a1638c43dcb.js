ed7ca2c9ef2519fd5764d1de1c865026afa045bf
Taurus <taoluwork@gmail.com>
2019-01-28 16:28:30 -0600

    update ignore

---
+++ bcai_v2.0/client/build/precache-manifest.64665f55096e29913b6e5a1638c43dcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f91d0bd4f59f0c3c175",
+    "url": "/static/css/main.b87b1967.chunk.css"
+  },
+  {
+    "revision": "0f91d0bd4f59f0c3c175",
+    "url": "/static/js/main.0f91d0bd.chunk.js"
+  },
+  {
+    "revision": "84d8cf0852b50142d511",
+    "url": "/static/js/1.84d8cf08.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75bb797f5ed4ba3c6e154927f20ec73f",
+    "url": "/index.html"
+  }
+];
