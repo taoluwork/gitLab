33db61eff3075490b7a4aebaa283ea6f08e17d0e
Taurus <taoluwork@gmail.com>
2019-06-12 13:36:30 -0500

    add truffle config for ropsten testnet

--- bcai_v2.0/truffle.js
+++ bcai_v2.0/truffle.js
@@ -1,5 +1,12 @@
 const path = require("path");
 
+//[Tutorial] How to deploy contract to Ropsten using Truffle
+//https://www.trufflesuite.com/tutorials/using-infura-custom-provider
+//my secret for testing: pencil month wasp omit salad owner ride armor install erupt neglect chimney
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "pencil month wasp omit salad owner ride armor install erupt neglect chimney";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -13,6 +20,13 @@ module.exports = {
       websockets: true
       //added by Taurus see link: https://github.com/trufflesuite/truffle/issues/1254
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/759cf07e10b84410975b28c5b6818041");
+      },
+      network_id: '3',
+    }
+   
   },
   contracts_build_directory: path.join(__dirname, "client/src/contracts")
 };
