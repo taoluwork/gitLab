962ca9e69db6c723834d81a7489f3c6940d5f155
PedroGRivera <pedrogrivera97@gmail.com>
2019-07-01 12:04:01 -0500

    added new word doc

---
+++ bcai_v2.0/client/build/precache-manifest.d69c43aca67adda71ab66fc0beb5f6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67816bc4be0b15c37a46",
+    "url": "/static/css/main.432193a2.chunk.css"
+  },
+  {
+    "revision": "67816bc4be0b15c37a46",
+    "url": "/static/js/main.67816bc4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1d1f3664dd23b972201e",
+    "url": "/static/css/2.b22e549d.chunk.css"
+  },
+  {
+    "revision": "1d1f3664dd23b972201e",
+    "url": "/static/js/2.1d1f3664.chunk.js"
+  },
+  {
+    "revision": "a42b789d7fd357c3b3dc36d1180639ff",
+    "url": "/index.html"
+  }
+];
