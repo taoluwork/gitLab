6b3f178af3058b15c58f621623f7753e47ae881e
PedroGRivera <pedrogrivera97@gmail.com>
2019-07-02 14:57:49 -0500

    refactoring of localEnv.js, but some minor bugs still persist

--- ML/localUser/localEnv.js
+++ ML/localUser/localEnv.js
@@ -1,362 +1,224 @@
-//get the neccessary libaries
 const util = require("util");
 const {exec} = require('child_process');
 const fs = require('fs');
-const EventEmitter = require('events');
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-var version = '';//tensorflow version
-var name = '';//name of python file
-var nameA= '';
-var emit;
-var f = 0;
-var evalFlag = 0;
-var pyFlag  = 0;
-var remFlag = 0;
-var runFlag = 0;
-var provider;
-var mode;
-var buffer; 
-var ip ;
-var user;
-var transmitCount = 0;
-var conns = new Object();
-conns.count = 0;
-conns.startTime = [];
-conns.connID = [];
-conns.connType = [];
-conns.status = [];
-////////////////////////////////////////////////////// server section  ////////////////////////////////////////////////////////////////////////
 
-if(process.argv[3] === undefined){
-  console.log("Invalid format, must include a valid IP address")
-  return;
-}
-else{
-  ip = process.argv[3];
-}
-
-io.on('connection', function(socket){
-  conns.startTime[conns.count] = Date.now();
-  conns.connID[conns.count] = socket.id;
-  conns.status[conns.count] = false; //becomes true when there is a finish
-  conns.count++;
-  console.log("NEW CONNECTION at time:" + conns.startTime[conns.count-1] + " from socket.id:" + conns.connID[conns.count-1]);
-
-  socket.emit("whoAmI", ip); // this assumes that the person can put a valid ip (this can be checked by some how parsing ifconfig bash command for this input)
-  socket.on('data', function(msg){
-    console.log("Data recieved sending to be ran...");
-    fs.writeFileSync("data.zip",msg, (err) => {
-      if(err){
-        console.log(err);
-      }
-    });
-    exec('unzip data.zip' , (err,stdout,stderr)=>{
-      if(err){
-        console.log(err);
-        return;
-      }
-      console.log(stdout);
-    });
-  });
-
-  socket.on('result', function(msg){
-    fs.writeFileSync("result.zip", msg, (err) => {
-      if(err){
-        console.log(err);
-      }
-    });
-    if(user){
-      exec('mv result.zip ~/Downloads' , (err,stdout,stderr)=>{
-        if(err){
-          console.log(err);
-          return;
-        } 
-        console.log(stdout);
-      });
-    }
-  });
-  socket.on("setupBuffer", msg => {
-    buffer = msg;
-    console.log(typeof msg);
-  });
-  socket.on("setupMode", msg => {
-    mode = msg;
-    /*user = true;
-    provider = false;
-    if(msg === "WORKER"){
-      user = false;
-      provider = true;
-    }*/
-  });
-  socket.on('request', (msg) =>{
-    console.log("Got:request and msg:" + msg);
-    var tag = "data";
-    var type = "WORKER";
-    if(mode === "WORKER"){
-      tag = "result";
-      type = "VALIDATOR";// or this can be a user gettign the last data maybe base it off of total connection count
-    }
-    if(buffer !== undefined){
-      socket.emit('transmitting' + msg, tag, buffer); 
-      console.log("emit:transmitting to:" + msg + " with tag:" + tag + " this.state.buffer:");
-    }
-    else{
-      console.log("NO FILE FOUND!!", "Please put the results within the field.", "warning");
-    }
-    socket.emit('fin'+ msg , tag);
-    console.log("emit:fin to:" + msg + " with tag:" + tag);
-  });
-  socket.on('recieved', (msg) => {
-    console.log("message was recieved");  
-    /*if(mode === 'WORKER'){
-      transmitCount +=1;
-      if(transmitCount >= 4){
-        //////////////remove stuff not sure if im done yet or not
-        buffer = undefined;
-        mode = undefined;
-        socket.emit("release" + ip);
-      }
-    }*/          
-  });
-
-
-});
-
-
-http.listen(3001 , function(){
-  console.log('listening on: ' + ip);
-});
-
-////////////////////////////////////////////////////// parsing section ////////////////////////////////////////////////////////////////////////
+var version = '';
+var name = '';
+var mode = ''; 
+var ip;
+var training = false;
+//////////////////////////////////////////////////////////////////////input section////////////////////////////////////////////////////////////////////////////////
+//modes
+//0-provider
+//1-validator
+//2-user
+//soon will be replaced in automation update for emits from the socket
 if(process.argv[2] === "--provider"){
-  provider = true;
+    mode = 0;
 }
 else if(process.argv[2] === "--validator"){
-  provider = false;
-}
+    mode = 1;
+  }
 else if(process.argv[2] === "--user"){
-  user = true;
+    mode = 2;
+  }
+else{
+    console.log("INVALID INPUT (--provider/--validator)");
+    return;
+}
+if(process.argv[3] === undefined){
+    console.log("Invalid format, must include a valid IP address")
+    return;
 }
 else{
-  console.log("INVALID INPUT (--provider/--validator)");
-  return;
+    ip = process.argv[3];
 }
-//run the file from the downloads
-function run(file, version){
-    exec('sudo docker run -i --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow:'+version+' python ./'+ file, (err,stdout,stderr)=>{ 
+//////////////////////////////////////////////////////////////////////server section/////////////////////////////////////////////////////////////////////////////
+  io.on('connection', function(socket){
+//    conns.startTime[conns.count] = Date.now();
+//    conns.connID[conns.count] = socket.id;
+//    conns.status[conns.count] = false; //becomes true when there is a finish
+//    conns.count++;
+//    console.log("NEW CONNECTION at time:" + conns.startTime[conns.count-1] + " from socket.id:" + conns.connID[conns.count-1]);
+  
+    socket.emit("whoAmI", ip); // this assumes that the person can put a valid ip (this can be checked by some how parsing ifconfig bash command for this input)
+    socket.on('data', function(msg){
+      console.log("Data recieved sending to be ran...");
+      console.log(msg);
+      fs.writeFile("data.zip",msg, (err) => {
+        if(err){
+          console.log(err);
+        }
+      });
+    });
+    socket.on('result', function(msg){
+      fs.writeFileSync("result.zip", msg, (err) => {
+        if(err){
+          //console.log(err);
+        }
+      });
+    });
+    socket.on("setupBuffer", msg => {
+      buffer = msg;
+    });
+    socket.on("setupMode", msg => { //partial complete future function
+      console.log('setupmode');  
+      /*if(process.argv[2] === "WORKER"){
+            mode = 0;
+        }
+        else if(process.argv[2] === "WORKER"){
+            mode = 1;
+        }
+        else if(process.argv[2] === "USER"){
+            mode = 2;
+        }*/
+    });
+    socket.on('request', (msg) =>{
+      console.log("Got:request and msg:" + msg);
+      var tag = "data";
+      var type = "WORKER";
+      if(mode === 0){
+        tag = "result";
+        type = "VALIDATOR";
+      }
+      if(buffer !== undefined){
+        socket.emit('transmitting' + msg, tag, buffer); 
+        console.log("emit:transmitting to:" + msg + " with tag:" + tag + " this.state.buffer:");
+      }
+      else{
+        console.log("NO FILE FOUND!!", "Please put the results within the field.", "warning");
+      }
+      socket.emit('fin'+ msg , tag);
+      console.log("emit:fin to:" + msg + " with tag:" + tag);
+    });
+    socket.on('recieved', (msg) => {
+      console.log("message was recieved");          
+    });
+});
+
+http.listen(3001 , function(){
+    console.log('listening on: ' + ip);
+});
+///////////////////////////////////////////////////////////////execution functions/////////////////////////////////////////////////////////////////////////////////////
+async function run(file, version){
+    await exec('sudo docker run -i --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow:'+version+' python ./'+ file, (err,stdout,stderr)=>{ 
       if(err){
-        console.log(err);
         return;
       }
       console.log(stdout);
+      if(mode === 0){
+        rem(name);
+      }
     });
 }
-
-function genFiles(fname){
-    if(provider){
-        exec('sudo docker run -i --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow:1.12.0 python train.py ' + fname , (err,stdout,stderr)=>{
+async function unzipF(file){
+    await exec('unzip ' + file , (err,stdout,stderr)=>{
         if(err){
-            console.log(err);
+            //console.log(err);
+            //return;
+        }
+        console.log(stdout);
+    });
+}
+async function genFiles(file){
+    await exec('sudo docker run -i --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow:1.12.0 python train.py ' + file , (err,stdout,stderr)=>{
+        if(err){
             return;
         }
         console.log(stdout);
-        });
-    }
-    else{
-        exec('unzip ' + fname , (err,stdout,stderr)=>{
-            if(err){
-              console.log(err);
-            return;
-            }
-            console.log(stdout);
-        });
-    }
+    });
 }
-
-function comp(){
-    if(!provider){
-        exec('sudo docker run -i --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow:1.12.0 python  comp.py ' , (err,stdout,stderr)=>{
+async function comp(){
+    if(mode !== 0){
+        await exec('sudo docker run -i --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow:1.12.0 python  comp.py ' , (err,stdout,stderr)=>{
             if(err){
-              console.log(err);
               return;
             }
             console.log(stdout);
         });
     }
 }
-
-function rem(){
-    if(provider){
-        exec('rm execute.py' , (err,stdout,stderr)=>{
+async function rem(file){
+    if(mode === 0){
+        await exec('rm execute.py' , (err,stdout,stderr)=>{
             if(err){
-              console.log(err);
               return;
             }
             console.log(stdout);
           });
           exec('zip result.zip result.h5 result.txt eval.py version.json' , (err,stdout,stderr)=>{
             if(err){
-              console.log(err);
               return;
             }
             console.log(stdout);
-            //console.log(nameA);
-            exec('rm result.h5 result.txt eval.py version.json '+nameA , (err,stdout,stderr)=>{
+            exec('rm result.h5 result.txt eval.py version.json data.zip '+ file , (err,stdout,stderr)=>{
                 if(err){
-                  console.log(err);
                   return;
                 }
+                training = false;
+                version = '';
+                name = '';
                 console.log(stdout);
             });
-            nameA = '';
           }); 
     }
     else{
-        exec('zip fin.zip result.h5 fin.txt' , (err,stdout,stderr)=>{
-            if(err){
-              console.log(err);
-              return;
-            }
-            console.log(stdout);
-            //console.log(nameA);
-            exec('rm result.h5 result.txt fin.txt eval.txt version.json eval.py '+nameA , (err,stdout,stderr)=>{
+        await    exec('rm result.h5 result.txt version.json eval.py result.zip eval.txt' , (err,stdout,stderr)=>{
                 if(err){
-                  console.log(err);
                   return;
                 }
                 console.log(stdout);
             });
-            nameA = '';
-          }); 
     }
 }
-
-function genEmit(){
-    const emitter = new EventEmitter();
-      emitter.once('getFile', (fname)=>{
-        name = fname;
-        //console.log(name)
-      });
-      emitter.once('getVer', (path)=>{
-        var obj = JSON.parse(fs.readFileSync(path , 'utf8'));
-        version = obj.ver;
-      });
-      emitter.once('runProg', (ver, nam)=>{
-        run(nam,ver);
-      });
-      emitter.once('resetVal',()=>{
-        version = '';
-        name = '';
-        f=0;
-      });
-      return emitter
+async function getVer(file){
+    var obj = await JSON.parse(fs.readFileSync(file , 'utf8'));
+    version = obj.ver;
 }
-
-function runEmits(){
-    emit.emit('runProg',version,name);
-    emit.emit('resetVal');
-    f=0;
-    evalFlag = 0;
-    remFlag = 0;
-    pyFlag = 0;
-    runFlag = 0;
-}
-if(user){
-  console.log("Welcome user!!!! Please wait for your results. Please wait for your results to appear in your downloads folder");
-}
-else if(provider){
-    //watch for there to be a change in the current file
-    fs.watch('.', (event, fname)=>{
-        if(f === 0){
-          emit = genEmit();
-          f = 1;
+/////////////////////////////////////////////////////////////////////////////file management section//////////////////////////////////////////////////////////////////////
+fs.watch('.', (event, file)=>{
+    //user mode case
+    if(event === 'change' && file === 'result.zip' && mode === 2){ //user recieves files
+        exec('mv result.zip ~/Downloads' , (err,stdout,stderr)=>{
+            if(err){
+              //console.log(err);
+              //return;
+            } 
+            console.log(stdout);
+          });
+    }
+    //validator mode cases
+    else if(event === 'change' && file === 'result.zip' && mode === 1){
+        unzipF(file);
+    }
+    else if(event === 'change' && file === 'version.json' && mode === 1){
+        getVer(file).then(run('eval.py',version));
+    }
+    else if(event === 'change' && file === 'eval.txt' && mode == 1){
+        comp();
+    }
+    else if(event === 'change' && file === 'fin.txt' && mode === 1){
+        rem('')
+    }
+    //provider mode cases
+    else if(event === 'change' && file === 'data.zip' && mode === 0){
+        unzipF(file);
+    }
+    else if(event === 'change' && file.search('.py') >= 0 && file != 'execute.py' && file != 'eval.py' &&  mode === 0){
+        genFiles(file); 
+        name = file;
+    }
+    else if(event === 'change' && file === 'version.json' && mode === 0 && version === ''){
+        getVer(file);
+    }
+    else if(event === 'change' && file === 'execute.py' && mode === 0){
+        if(training === false){
+            training = true;
+            run(file, version);
         }
-        //when the unprepared file is loaded
-        if(event === 'change' && fname.search('.py') >= 0 && fname != 'execute.py' && fname!= 'eval.py'){
-            if(pyFlag === 0){
-                console.log("generting files");
-                genFiles(fname);
-                nameA = fname;
-                pyFlag = 1;
-            }
-        }
-        //when the file is loaded
-        if(event === 'change' && fname === 'execute.py' && name === ''){
-          console.log("getting execute.py");
-          emit.emit('getFile', fname);
-        }
-        //when the version is loaded
-        if(event === 'change' && fname === 'version.json' && version === ''){
-          console.log("getting version.json");
-          emit.emit('getVer', fname);
-        }
-        //when result.h5 is made remove the generated stuff
-        if(event === 'change' && fname === 'result.txt'){
-                if(remFlag === 0){
-                console.log("removing generated files and zipping");
-                rem();
-                remFlag=1;
-            }
-        }
-        //when both have been loaded
-        if(version != '' && name != ''){
-          if(runFlag === 0){
-            console.log('Executing TF code');
-            setTimeout(runEmits, 1000);
-            runFlag = 1;
-          }
-        }
-    });
-}
-else{
-    //watch for there to be a change in downloads
-    fs.watch('.', (event, fname)=>{
-        if(f === 0){
-          emit = genEmit();
-          f = 1;
-        }
-        //when the unprepared file is loaded
-        if(event === 'change' && fname=== 'result.zip'){
-          if(pyFlag === 0){
-            console.log("generting files");
-            genFiles(fname);
-            nameA = fname;
-            pyFlag = 1;
-          }
-        }
-        //when the file is loaded
-        if(event === 'change' && fname === 'eval.py' && name === ''){
-          console.log("getting eval.py");
-          emit.emit('getFile', fname);
-        }
-        //when the version is loaded
-        if(event === 'change' && fname === 'version.json' && version === ''){
-          console.log("getting version.json");
-          emit.emit('getVer', fname);
-        }
-        //when result.h5 is made remove the generated stuff
-        if(event === 'change' && fname === 'eval.txt'){
-          if(evalFlag === 0){
-                console.log("comparing");
-                comp();
-                evalFlag=1;
-            }
-        }
-        //when result.h5 is made remove the generated stuff
-        if(event === 'change' && fname === 'fin.txt'){
-            if(remFlag === 0){
-                console.log("removing generated files and zipping");
-                rem();
-                remFlag=1;
-            }
-        }
-        //when both have been loaded
-        if(version != '' && name != ''){
-          console.log('Executing TF code');
-          setTimeout(runEmits, 1000);
-        }
-      });  
-}
+    }
+});
